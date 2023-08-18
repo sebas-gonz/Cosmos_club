a511b28cf2c79f31657d0f75c9945bd86be16999
Sebastián González <sebastian.javier.gon@gmail.com>
2023-08-17 21:04:06 -0400

    Creacion del proyecto Cosmos Club

---
+++ src/main/java/com/cosmosclub/ComosClubApplication.java
@@ -0,0 +1,13 @@
+package com.cosmosclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComosClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComosClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cosmosclub/ComosClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmosclub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComosClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
